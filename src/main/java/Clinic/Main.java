0139ba01a182344cea7f18af7601d5f0e92e07d3
Alesia <andomyel@yandex.ru>
2023-09-18 00:29:57 +0300

    HomeWork2/3

--- src/main/java/Clinic/Main.java
+++ src/main/java/Clinic/Main.java
@@ -21,16 +21,16 @@ public class Main {
                 LocalDate.of(1994, 12, 25), Specialization.THERAPIST, irina);
         Cat barsik = new Cat("barsik", 10.0, Anna, LocalDate.now(), List.of(chumka), Color.BLACK, "siamskiy");
         Dog barbos = new Dog("barbos", 10.0, Anna, LocalDate.now(), List.of(chumka), Color.WHITE);
-        Parrot kesha = new Parrot("kesha", 0.2, new Owner("Larisa"), LocalDate.now(),Color.BLACK);
-        Penguin seva = new Penguin("seva", 30.0, new Owner("Piter"), LocalDate.now(),Color.BLACK);
-        Fish nemo = new Fish("nemo", 0.5, new Owner("Masha"), LocalDate.now(),Color.WHITE);
-        Frog ignat = new Frog("ignat", 0.1, new Owner("Fedor"), LocalDate.now(),Color.BLACK);
-        Spider mrak = new Spider("mrak", 0.2, new Owner("Anna"), LocalDate.now(),Color.BLACK);
+        Parrot kesha = new Parrot("kesha", 0.2, new Owner("Larisa"), LocalDate.now(), Color.BLACK);
+        Penguin seva = new Penguin("seva", 30.0, new Owner("Piter"), LocalDate.now(), Color.BLACK);
+        Fish nemo = new Fish("nemo", 0.5, new Owner("Masha"), LocalDate.now(), Color.WHITE);
+        Frog ignat = new Frog("ignat", 0.1, new Owner("Fedor"), LocalDate.now(), Color.BLACK);
+        Spider mrak = new Spider("mrak", 0.2, new Owner("Anna"), LocalDate.now(), Color.BLACK);
         irina.inspect(barbos);
         igor.giveTaskNurse(barsik);
         System.out.println(barsik.getName() + " убегает от медсестры со скоростью " + barsik.getRunSpeed());
 
-        Clinic clinic = new Clinic(List.of (barsik, barbos, kesha, seva, nemo, ignat, mrak));
+        Clinic clinic = new Clinic(List.of(barsik, barbos, kesha, seva, nemo, ignat, mrak));
         System.out.printf("%nЛетающие животные: %s", clinic.getAllFlyed());
         System.out.printf("%nБегающие животные: %s", clinic.getAllRunable());
         System.out.printf("%nПлавающие животные: %s", clinic.getAllSwimable());
@@ -39,11 +39,12 @@ public class Main {
         printAnimalsThreeVarMove(clinic);
 
     }
+
     public static void printAnimalsThreeVarMove(Clinic clinic) {
         if (clinic.getAnimalsThreeVarMove().size() == 0) {
             System.out.printf("%nНет животных, умеющих и летать, и плавать, и бегать");
         } else {
-            System.out.printf("%nПлавающие, бегающие, летающие животные: %s",clinic.getAnimalsThreeVarMove());
+            System.out.printf("%nПлавающие, бегающие, летающие животные: %s", clinic.getAnimalsThreeVarMove());
         }
     }
 }
